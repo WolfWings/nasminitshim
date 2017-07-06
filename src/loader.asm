13a62389be929bd4e6853034567933557bffb55e
Wolf <wolfwings@gmail.com>
2017-07-06 05:40:37 -0500

    Code overhaul for clarity and streamlining main assembly file

--- src/loader.asm
+++ src/loader.asm
@@ -1,84 +1,64 @@
 %include "constants.inc"
 %include "syscall.inc"
-
-%macro alignz 1
-	align %1, db 0
-%endmacro
-
-%macro print 2+
-	[section .data]
-%%str:	db	%2
-%%ptr:	alignz 16
-	__SECT__
-
-	sys_write(%1, %%str, %%ptr - %%str)
-%endmacro
-
-%macro error_check 1+
-	cmp	rax,	0
-	jge	%%skip
-	mov	[errno],	rax
-	sys_write(STDERR, error_banner, error_banner.len)
-	print STDERR, "37m ", %1, " "
-	sys_write(STDERR, error_banner, error_banner.len2)
-	jmp	__abort
-%%skip:
-%endmacro
-
-section .bss align=16
-errno: resq 1
-
-section .data align=16
-squashfs: db "squashfs", 0
-	alignz 16
-
-nullstring: db 0
-	alignz 16
-
 %include "errors.inc"
 
 section .text
 
+	[section .data]
+	alignz 16
+squashfs: db "squashfs"
+nullstring: db 0
+	__SECT__
+
+	; Calling ABI:
+	;
+	;          * PUSH these parameters onto the stack
+	; rsp + 40 * Pointer to SquashFS filename, zero terminated
+	; rsp + 32 * Pointer to loopback filename, zero terminated
+	; rsp + 24 * Pointer to mountpoint, zero terminated
+	; rsp + 16 - RET address
+	; rsp +  8 - SquashFS File Handle
+	; rsp      - Loopback Device Number
 	alignz 16
 __loopback_mount:
 	sub	rsp,	16
 	sys_open([rsp + 40], O_CLOEXEC | O_RDONLY, 0)
 	cmp	rax,	ENOENT
 	jne	.0
-	print STDOUT, "Unable to find squashfs file, skipping mount."
+	print STDOUT, "No squashfs, skipping mount."
 	jmp	.skip
-.0:	error_check "Unable to open squashfs file!"
+.0:	error_check "Can't open squashfs!"
 
 	mov	[rsp + 8], rax
 
 	sys_ioctl([loopcontrol], LOOP_CTL_GET_FREE, 0)
-	error_check "Unable to allocate loopback device!"
+	error_check "Can't allocate loopback!"
 
 	or	rax,	7 << 8
 	mov	[rsp], rax
 
 	sys_mknod([rsp + 32], S_IFBLK | 0600O, [rsp])
-	error_check "Unable to create loopback device!"
+	error_check "Can't create loopback!"
 
 	sys_open([rsp + 32], O_CLOEXEC | O_RDWR, 0)
-	error_check "Unable to open loopback device!"
+	error_check "Can't open loopback!"
 
 	mov	[rsp], rax
 
 	sys_ioctl([rsp], LOOP_SET_FD, [rsp + 8])
-	error_check "Unable to attach squashfs to loopback device!"
+	error_check "Can't attach squashfs to loopback!"
 
 	sys_close([rsp + 8])
-	error_check "Unable to close squashfs file!"
+	error_check "Can't close squashfs!"
 
 	sys_close([rsp])
-	error_check "Unable to close loopback device!"
+	error_check "Can't close loopback!"
 
 	sys_mount([rsp + 32], [rsp + 24], squashfs, MS_RDONLY, nullstring)
-	error_check "Unable to mount /usr!"
+	error_check "Can't mount loopback!"
 
 	sys_unlink([rsp + 32])
-	error_check "Unable to unlink loopback device!"
+	error_check "Can't delete loopback!"
 
 .skip:
 	print STDOUT, 10, 13
@@ -87,40 +67,12 @@ __loopback_mount:
 
 
 
-	alignz 16
-__abort:
-	mov	rax,	[errno]
-	neg	rax
-	shl	rax,	4
-	cmp	rax,	errors.max
-	jae	.unknown
-	mov	rbx,	[errors + rax]
-	cmp	rbx,	0
-	je	.unknown
-	sys_write(STDERR, rbx, [errors + rax + 8])
-	jmp	_exit_error
-
-.unknown:
-	mov	rax,	[errno]
-	mov	ecx,	16
-.0:	mov	rdx,	rax
-	shr	rax,	4
-	and	edx,	15
-	mov	dl,	[hex + edx]
-	mov	[error_buffer.hex + rcx - 1], dl
-	dec	ecx
-	jnz	.0
-	sys_write(STDOUT, error_buffer, error_buffer.len)
-
-_exit_error:
-	sys_exit(1)
-
-
-
 	global _start:function
 	alignz 16
 _start:
 	print STDOUT, "PocketInit", 10, 13
+
+	print STDOUT, "Creating loopback control device..."
 
 	[section .data]
 loopcontrolfilename: db "loop-control", 0
@@ -131,17 +83,22 @@ loopcontrol: resq 1
 	__SECT__
 
 	sys_mknod(loopcontrolfilename, S_IFCHR | 0600O, ((10 << 8) + 237))
-	error_check "Unable to create loop-control device!"
+	error_check "Unable to create!"
 
 	sys_open(loopcontrolfilename, O_CLOEXEC | O_RDWR, 0)
-	error_check "Unable to open loop-control device!"
+	error_check "Unable to open!"
 
 	mov	[loopcontrol], rax
 
 	sys_unlink(loopcontrolfilename)
-	error_check "Unable to unlink loop-control device!"
+	error_check "Unable to delete!"
+
+	print STDOUT, 10, 13
 
 	[section .data]
+usrsquashfsfilenamenew: db "srv/read-only/new-usr.squashfs", 0
+	alignz 16
+
 usrsquashfsfilename: db "srv/read-only/usr.squashfs", 0
 	alignz 16
 
@@ -153,17 +110,26 @@ usrmountpoint: db "usr", 0
 	__SECT__
 
 _usr:
-	print STDOUT, "Mounting /usr..."
+	print STDOUT, "Setting up /usr..."
 
-	push	usrsquashfsfilename	; rsp + 40
-	push	usrloopdevfilename	; rsp + 32
-	push	usrmountpoint		; rsp + 24
+	sys_rename(usrsquashfsfilenamenew, usrsquashfsfilename)
+	cmp	rax,	0
+	jne	.noupdate
+	print STDOUT, "Updated version..."
+.noupdate:
+
+	push	usrsquashfsfilename
+	push	usrloopdevfilename
+	push	usrmountpoint
 	call	__loopback_mount
 	add	rsp,	24
 
 
 
 	[section .data]
+lib64squashfsfilenamenew: db "srv/read-only/new-lib64.squashfs", 0
+	alignz 16
+
 lib64squashfsfilename: db "srv/read-only/lib64.squashfs", 0
 	alignz 16
 
@@ -175,11 +141,17 @@ lib64mountpoint: db "lib64/", 0
 	__SECT__
 
 _lib64:
-	print STDOUT, "Mounting /lib64..."
+	print STDOUT, "Setting up /lib64..."
 
-	push	lib64squashfsfilename	; rsp + 40
-	push	lib64loopdevfilename	; rsp + 32
-	push	lib64mountpoint		; rsp + 24
+	sys_rename(lib64squashfsfilenamenew, lib64squashfsfilename)
+	cmp	rax,	0
+	jne	.noupdate
+	print STDOUT, "Updated version..."
+.noupdate:
+
+	push	lib64squashfsfilename
+	push	lib64loopdevfilename
+	push	lib64mountpoint
 	call	__loopback_mount
 	add	rsp,	24
 
@@ -199,9 +171,9 @@ hiddenmountpoint: db "srv/read-only", 0
 _hidden:
 	print STDOUT, "Hiding /srv/read-only..."
 
-	push	hiddensquashfsfilename	; rsp + 40
-	push	hiddenloopdevfilename	; rsp + 32
-	push	hiddenmountpoint	; rsp + 24
+	push	hiddensquashfsfilename
+	push	hiddenloopdevfilename
+	push	hiddenmountpoint
 	call	__loopback_mount
 	add	rsp,	24
 
@@ -229,7 +201,9 @@ env:	dq env_home, env_term, 0
 	alignz 16
 	__SECT__
 
+	print STDOUT, "Transfering control to /sbin/init via execve...", 10, 13
+
 	sys_execve(execve_filename, argv, env)
-	error_check "Unable to transfer control to /sbin/init via execve!"
+	error_check "Can't!"
 
 	sys_exit(2)
